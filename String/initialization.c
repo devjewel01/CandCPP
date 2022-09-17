144aaf8c30e007b8b19719d80210376fb224e50b
devjewel01 <devjewel01@stud.cou.ac.bd>
2022-09-17 15:45:26 +0600

    string add

---
+++ String/initialization.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    /*
+    It is string initialization
+     char a[] = {'a', 'b', 'c', '\0'};
+    char a[2] = {'a', 'b', 'c', '\0'};
+    char a[] = "abc"; 
+    */
+    char a[20] = "you are czsian"; // 4 method
+    
+    
+
+    // printf("%s\n", a); // output string
+    puts(a);
+
+
+}
